54ee6092587f1a7b325ee03e874775387053e63a
Mark Carrier <markcarrier@gmail.com>
2020-11-10 12:00:10 +0300

    Fixes some issues with repainting of ground tiles, adds some refactoring and mouse click capture

--- src/staging.js
+++ src/staging.js
@@ -1,5 +1,6 @@
 import {
   animationFrameScheduler,
+  BehaviorSubject,
   defer,
   fromEvent,
   interval,
@@ -14,43 +15,64 @@ import {
   tap,
   withLatestFrom
 } from 'rxjs/operators'
-
 import { keyNames } from './keys.util'
 import * as tilemapData from './tilemap-data.json'
 
-export function setScene(renderer, gameState) {
+export function setScene(renderer, initialGameState) {
   return new Promise((resolve, reject) => {
-    if (gameState.scene === 'none') {
+    if (initialGameState.scene === 'none') {
       console.log('No scene to render')
-      return gameState
+      return initialGameState
     }
+    console.log(`Changing scene to ${initialGameState.scene}`)
 
-    const gameState$ = of(gameState)
+    const gameState$ = new BehaviorSubject({})
+    gameState$.next({
+      ...initialGameState,
+      player: {
+        x: 0,
+        y: 0
+      }
+    })
 
     var stats = new Stats()
     stats.showPanel(0)
     document.body.appendChild(stats.dom)
 
     const atlasName = 'atlas3.json'
-    const groundTileName = 'medievalTile_58.png'
+    const groundTileNames = [
+      'medievalTile_58.png',
+      'medievalTile_01.png',
+      'medievalTile_13.png',
+      'medievalTile_14.png',
+      'medievalTile_15.png',
+      'medievalTile_16.png'
+    ]
     const treeTileNames = new Array(8)
       .fill(0)
       .map((_, index) => `medievalTile_${index + 41}.png`)
     const unitTileName = 'medievalUnit_24.png'
     const tileSize = 128
-    const resX = gameState.canvasSettings.resolutionX
-    const resY = gameState.canvasSettings.resolutionY
+    const canvasSize = {
+      x: initialGameState.canvasSettings.resolutionX,
+      y: initialGameState.canvasSettings.resolutionY
+    }
     const playerHeight = 33
     const playerWidth = 48
-    const playerOffsetX = resX / 2 - playerHeight / 2
-    const playerOffsetY = resY / 2 - playerWidth / 2
-    let tilemap = null
-    let player = {
-      x: 0,
-      y: 0
+    const playerWalkingSpeed = 0.4
+    const playerOffset = {
+      x: canvasSize.x / 2 - playerHeight / 2,
+      y: canvasSize.y / 2 - playerWidth / 2
     }
 
-    console.log(`Changing scene to ${gameState.scene}`)
+    const numberOfOutOfScreenTiles = 6
+    const totalNumberOfTilesToPaint = {
+      x: parseInt(canvasSize.x / tileSize) + numberOfOutOfScreenTiles,
+      y: parseInt(canvasSize.y / tileSize) + numberOfOutOfScreenTiles
+    }
+
+    let tilemap = null
+
     let loader = new PIXI.Loader()
     loader.add(atlasName)
 
@@ -62,7 +84,7 @@ export function setScene(renderer, gameState) {
       map((e) => ({ code: e.keyCode, direction: 'up' }))
     )
 
-    const keyHolds$ = merge(keyDowns$, keyUps$).pipe(
+    const keyHolds$ = merge(keyDowns$, keyUps$, of({})).pipe(
       scan((acc, next) => {
         let newAcc = acc.filter((v) => v !== next.code)
         if (next.direction == 'down') return newAcc.concat(next.code)
@@ -75,24 +97,36 @@ export function setScene(renderer, gameState) {
       )
     )
 
+    fromEvent(document, 'click')
+      .pipe(
+        map((mouseClick) => ({ x: mouseClick.clientX, y: mouseClick.clientY })),
+        withLatestFrom(gameState$),
+        map(([mouseClick, gameState]) =>
+          processTargetClick(mouseClick, gameState)
+        )
+      )
+      .subscribe((gameState) => gameState$.next(gameState))
+
     loader.load(setup)
 
     function setup(loader, resources) {
       tilemap = createTileMap(resources)
       const atlasTextures = resources[atlasName].textures
-      drawGroundTiles(atlasTextures)
       addPlayer(atlasTextures)
       const makeFrame = createFramemaker(atlasTextures)
 
-      let frames$ = buildAnimationFrameClock().pipe(
+      const frames$ = buildAnimationFrameClock().pipe(
         withLatestFrom(keyHolds$, gameState$),
-        map(([deltaTime, keysDown, gameState]) =>
-          makeFrame(deltaTime, gameState, keysDown)
-        )
+        map(([deltaTime, keyHolds, gameState]) =>
+          makeFrame(deltaTime, gameState, keyHolds)
+        ),
+        tap((gameState) => gameState$.next(gameState))
       )
 
       frames$.subscribe(
-        () => {},
+        (gameState) => {
+          window.gameState = gameState
+        },
         (error) => reject(error),
         () => {
           resolve()
@@ -101,26 +135,51 @@ export function setScene(renderer, gameState) {
     }
 
     function createTileMap(atlasTextures) {
-      let tilemap = new PIXI.tilemap.CompositeRectTileLayer(
+      const tilemap = new PIXI.tilemap.CompositeRectTileLayer(
         0,
-        atlasTextures[groundTileName]
+        atlasTextures[groundTileNames[0]]
       )
       renderer.stage.addChild(tilemap)
       return tilemap
     }
 
-    function drawGroundTiles(atlasTextures) {
-      const numberOfTilesX = parseInt(resX / tileSize) + 6
-      const numberOfTilesY = parseInt(resY / tileSize) + 6
+    function processTargetClick(click, gameState) {
+      const distanceToTargetX = click.x - playerOffset.x
+      const distanceToTargetY = click.y - playerOffset.y
+      const target = {
+        x: gameState.player.x + distanceToTargetX,
+        y: gameState.player.y + distanceToTargetY
+      }
 
-      const groundOffsetX = player.x % tileSize
-      const groundOffsetY = player.y % tileSize
+      return {
+        ...gameState,
+        target
+      }
+    }
+
+    function paintGroundTiles(atlasTextures, gameState) {
+      const groundOffset = {
+        x: gameState.player.x % tileSize,
+        y: gameState.player.y % tileSize
+      }
+
+      let repaintCount = 0
+      if (gameState.lastRepaintPos)
+        repaintCount = gameState.lastRepaintPos.count + 1
 
       tilemap.clear()
-      for (let x = -numberOfTilesX; x <= numberOfTilesX; x++) {
-        for (let y = -numberOfTilesY; y <= numberOfTilesY; y++) {
-          const tilePosX = (player.x - groundOffsetX) / tileSize + x
-          const tilePosY = (player.y - groundOffsetY) / tileSize + y
+      for (
+        let x = -totalNumberOfTilesToPaint.x;
+        x <= totalNumberOfTilesToPaint.x;
+        x++
+      ) {
+        for (
+          let y = -totalNumberOfTilesToPaint.y;
+          y <= totalNumberOfTilesToPaint.y;
+          y++
+        ) {
+          const tilePosX = (gameState.player.x - groundOffset.x) / tileSize + x
+          const tilePosY = (gameState.player.y - groundOffset.y) / tileSize + y
 
           let tileTexture
           const tileNameOverride =
@@ -129,7 +188,7 @@ export function setScene(renderer, gameState) {
           if (tileNameOverride) {
             tileTexture = atlasTextures[tileNameOverride]
           } else {
-            tileTexture = atlasTextures[groundTileName]
+            tileTexture = atlasTextures[groundTileNames[0]]
           }
           tilemap.addFrame(tileTexture, x * tileSize, y * tileSize)
 
@@ -145,16 +204,24 @@ export function setScene(renderer, gameState) {
       }
 
       tilemap.position.set(
-        playerOffsetX + player.x - groundOffsetX,
-        playerOffsetY + player.y - groundOffsetY
+        playerOffset.x + gameState.player.x - groundOffset.x,
+        playerOffset.y + gameState.player.y - groundOffset.y
       )
+
+      return {
+        ...gameState,
+        lastRepaintPos: {
+          ...gameState.player,
+          count: repaintCount
+        }
+      }
     }
 
     function addPlayer(atlasTextures) {
-      let playerTexture = atlasTextures[unitTileName]
-      let playerSprite = new PIXI.Sprite(playerTexture)
-      playerSprite.x = playerOffsetX
-      playerSprite.y = playerOffsetY
+      const playerTexture = atlasTextures[unitTileName]
+      const playerSprite = new PIXI.Sprite(playerTexture)
+      playerSprite.x = playerOffset.x
+      playerSprite.y = playerOffset.y
       renderer.stage.addChild(playerSprite)
     }
 
@@ -172,28 +239,73 @@ export function setScene(renderer, gameState) {
     }
 
     function createFramemaker(atlasTextures) {
-      const makeFrame = (deltaTime, state, inputState) => {
+      const makeFrame = (deltaTime, gameState, keyHolds) => {
         stats.begin()
-        if (
-          inputState.length &&
-          (player.x % (10 * tileSize) === 0 || player.y % (10 * tileSize) === 0)
-        ) {
-          drawGroundTiles(atlasTextures)
+
+        let nextState = {
+          ...gameState
         }
 
-        if (inputState.indexOf(keyNames.up_arrow) !== -1) player.y -= 4
-        else if (inputState.indexOf(keyNames.down_arrow) !== -1) player.y += 4
+        const distanceToTravel = deltaTime * playerWalkingSpeed
 
-        if (inputState.indexOf(keyNames.left_arrow) !== -1) player.x -= 4
-        else if (inputState.indexOf(keyNames.right_arrow) !== -1) player.x += 4
+        if (repaintIsNeeded(gameState)) {
+          nextState = paintGroundTiles(atlasTextures, nextState)
+        }
 
-        tilemap.pivot.set(player.x, player.y)
-        //requestAnimationFrame(gameLoop)
+        if (keyHolds.length) {
+          nextState.target = null
+          if (keyHolds.indexOf(keyNames.up_arrow) !== -1)
+            nextState.player.y -= distanceToTravel
+          else if (keyHolds.indexOf(keyNames.down_arrow) !== -1)
+            nextState.player.y += distanceToTravel
+
+          if (keyHolds.indexOf(keyNames.left_arrow) !== -1)
+            nextState.player.x -= distanceToTravel
+          else if (keyHolds.indexOf(keyNames.right_arrow) !== -1)
+            nextState.player.x += distanceToTravel
+        } else if (gameState.target) {
+        }
+
+        tilemap.pivot.set(nextState.player.x, nextState.player.y)
         stats.end()
 
-        return state
+        return nextState
       }
       return makeFrame
+    }
+
+    function repaintIsNeeded(gameState) {
+      if (!gameState.lastRepaintPos) return true
+
+      const paintedAreaLimits = {
+        left:
+          gameState.lastRepaintPos.x - tileSize * totalNumberOfTilesToPaint.x,
+        right:
+          gameState.lastRepaintPos.x + tileSize * totalNumberOfTilesToPaint.x,
+        top:
+          gameState.lastRepaintPos.y - tileSize * totalNumberOfTilesToPaint.y,
+        bottom:
+          gameState.lastRepaintPos.y + tileSize * totalNumberOfTilesToPaint.y
+      }
+
+      const buffer = tileSize
+
+      if (gameState.player.x - playerOffset.x < paintedAreaLimits.left + buffer)
+        return true
+      if (
+        gameState.player.x + playerOffset.x >
+        paintedAreaLimits.right - buffer
+      )
+        return true
+      if (gameState.player.y - playerOffset.y < paintedAreaLimits.top + buffer)
+        return true
+      if (
+        gameState.player.y + playerOffset.y >
+        paintedAreaLimits.bottom - buffer
+      )
+        return true
+
+      return false
     }
   })
 }
